a6a5463b9b2c2921a62f5d98b4096ecb213a6fea
gaarumon <k.kaulinskaite@gmail.com>
2026-03-27 14:01:44 +0100

    added demo guis for login and main

--- src/GUI/DemoGUI.java
+++ src/GUI/DemoGUI.java
@@ -8,9 +8,13 @@ import java.io.IOException;
 public class DemoGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DemoGUI.class.getResource("SceneForMain.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(DemoGUI.class.getResource("SceneForMain.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DemoGUI.class.getResource("SceneForLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        String css = this.getClass().getResource("styleSheet.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.setTitle("Recipeasy!");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/GUI/SceneController.java
@@ -0,0 +1,25 @@
+package GUI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMainMenu(ActionEvent event ) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("SceneForMain.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        String css = this.getClass().getResource("styleSheet.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
