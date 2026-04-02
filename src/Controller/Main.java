b300a7fd9ddccd669c9af2b1828bd0dcb403a0bb
gaarumon <k.kaulinskaite@gmail.com>
2026-04-02 10:29:51 +0200

    new Main

---
+++ src/Controller/Main.java
@@ -0,0 +1,10 @@
+package Controller;
+
+import GUI.DemoGUI;
+import javafx.application.Application;
+
+public class Main {
+    static void main(String[] args) {
+        Application.launch(DemoGUI.class, args);
+    }
+}
--- src/GUI/DemoGUI.java
+++ src/GUI/DemoGUI.java
@@ -9,12 +9,10 @@ import java.io.IOException;
 public class DemoGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(DemoGUI.class.getResource("SceneForMain.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(DemoGUI.class.getResource("SceneForLogin.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         String css = this.getClass().getResource("styleSheet.css").toExternalForm();
         scene.getStylesheets().add(css);
-        //stage.getIcons().add(new Image("/Users/elvirajensen/Desktop/Recipeasy/src/Images/logo.png"));
         stage.setScene(scene);
         stage.show();
     }
