f8e3c2478b690a0d1c18a741d2d7ffdced239ef8
elvirajensen <elvira.jensen@hotmail.com>
2026-04-07 11:33:52 +0200

    hejhej

--- src/Model/Database.java
+++ src/Model/Database.java
@@ -126,4 +126,44 @@ public class Database {
             throw e;
         }
     }
+
+    public boolean doesUsernameAlreadyExist(String username) throws Exception{
+        Connection con = getDatabaseConnection();
+        int count = 0;
+        try {
+            String QUERY = "SELECT COUNT(*) FROM appuser WHERE username = ?";
+            PreparedStatement pstmt = con.prepareStatement(QUERY);
+            pstmt.setString(1, username);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            count = rs.getInt(1);
+            rs.close();
+            pstmt.close();
+            con.close();
+            return count > 0;
+        } catch (Exception e) {
+            if (con != null) {
+                con.close();
+            }
+        }
+        return count > 0;
+    }
+
+    public void addNewUserToDatabase(String username, String password) throws Exception {
+        Connection con = getDatabaseConnection();
+        try {
+            String INSERT = "INSERT INTO appuser (username, pass_word) VALUES (?, ?)";
+            PreparedStatement pstmt = con.prepareStatement(INSERT);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            int rows = pstmt.executeUpdate();
+            System.out.println("You have been registered!");
+            pstmt.close();
+            con.close();
+        }catch (Exception e){
+            if (con!=null){
+                con.close();
+            }
+        }
+    }
 }
