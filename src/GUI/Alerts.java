5b558b79f8d867360d1e1ed18ce366cd85dac964
elvirajensen <elvira.jensen@hotmail.com>
2026-04-26 14:54:37 +0200

    Sign out implemented

--- src/GUI/Alerts.java
+++ src/GUI/Alerts.java
@@ -1,6 +1,9 @@
 package GUI;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
+import java.util.Optional;
 
 public class Alerts {
 
@@ -21,5 +24,16 @@ public class Alerts {
         alert.setContentText(alertMessage);
         alert.getDialogPane().getStylesheets().add(getClass().getResource("/GUI/styleSheet.css").toExternalForm());
         alert.showAndWait();
+    }
+
+    public boolean confirmDialog(String alertMessage){
+        Alert alert =new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("You are about to sign out");
+        alert.setHeaderText(null);
+        alert.setContentText(alertMessage);
+        alert.getDialogPane().getStylesheets().add(getClass().getResource("/GUI/styleSheet.css").toExternalForm());
+
+        Optional<ButtonType> result = alert.showAndWait();
+        return result.isPresent() && result.get() == ButtonType.OK;
     }
 }
